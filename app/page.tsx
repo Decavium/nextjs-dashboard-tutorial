c1707a639e99ddc965fb28d43cdc1a9d9e9ecc1c
Leo Williams <leokabirwilliams@gmail.com>
2024-02-08 20:20:42 +1100

    Finished course.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 // import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
